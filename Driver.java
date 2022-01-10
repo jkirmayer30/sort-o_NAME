6ce7185825e8d0af29579d0e5bbb5be113432251
jkirmayer30 <78454399+jkirmayer30@users.noreply.github.com>
2022-01-09 22:10:04 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -19,25 +19,44 @@ public class Driver{
   public static ArrayList<Comparable> makeRandom(int size){
     ArrayList glen = new ArrayList<Integer>();
     for (int i = 0;i<size;i++){
-      glen.add(Random.random());
+      glen.add((int) 100000*Math.random());
     }
     return glen;
   }
 
   public static void main(String[] args){
-    ArrayList glek =makeDescending(4);
-      ArrayList glem = new ArrayList<Integer>();
-        glem.add(7);
-        glem.add(1);
-        glem.add(5);
-        glem.add(12);
-        ArrayList glen = new ArrayList<Integer>();
-          glen.add(7);
-          glen.add(1);
-          glen.add(5);
-          glen.add(12);
-    Sorts.insertion(glem);
-    Sorts.bubble(glek);
-    Sorts.selection(glen);
-  }
+    int size = Integer.parseInt(args[0]);
+    int randos = Integer.parseInt(args[1]);
+    if (args[2].equals("bubble")){
+        System.out.println("Suspected best case:");
+        ArrayList glek =makeAscending(size);
+        Sorts.bubble(glek);
+        System.out.println("Random cases:");
+        for (int i = 0;i<randos;i++){
+          ArrayList glem = makeRandom(size);
+          Sorts.bubble(glem);
+        }
+        System.out.println("Suspected worst case:");
+        ArrayList glen = makeDescending(size);
+        Sorts.bubble(glen);
+    } else if (args[2].equals("insertion")){
+        System.out.println("Suspected best case:");
+        ArrayList glek =makeAscending(size);
+        Sorts.insertion(glek);
+        System.out.println("Random cases:");
+        for (int i = 0;i<randos;i++){
+           ArrayList glem = makeRandom(size);
+           Sorts.insertion(glem);
+        }
+        System.out.println("Suspected worst case:");
+        ArrayList glen = makeDescending(size);
+        Sorts.insertion(glen);
+    } else if (args[2].equals("selection")){
+              System.out.println("Random cases:");
+              for (int i = 0;i<randos;i++){
+                 ArrayList glem = makeRandom(size);
+                 Sorts.selection(glem);
+              }
+        }
+    }
 }
